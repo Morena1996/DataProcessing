5bf629bd71b846b9aeb564b72df30ef6c3a3515d
Morena1996 <mbastiaansen@hotmail.com>
2017-09-27 20:27:14 +0200

    .

--- Homework/week-3/barchart.js
+++ Homework/week-3/barchart.js
@@ -9,58 +9,71 @@ File with javascript code for the barchart of KNMI average temperatures data
 */
 
 // Read in JSON data
-d3.json("/KNMI_2015_DeBilt.json", function(data) {
-	
-	var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+        d3.json("/KNMI_2015_DeBilt.json", function(data) {
 
-	var x = d3.scale.ordinal()
-	    .rangeRoundBands([0, width], .1);
+            var margin = {top: 20, right: 10, bottom: 10, left: 20},
+                width = 3000 - margin.left - margin.right,
+                height = 500 - margin.top - margin.bottom;
 
-	var y = d3.scale.linear()
-	    .range([height, 0]);
+            console.log(width)
+            console.log(height)
+            console.log(margin)
 
-	var xAxis = d3.svg.axis()
-	    .scale(x)
-	    .orient("bottom");
+            var x = d3.scale.ordinal()
+                    .rangeRoundBands([0, width], .1);
 
-	var yAxis = d3.svg.axis()
-	    .scale(y)
-	    .orient("left");
+            console.log(x)
 
-	var chart = d3.select(".chart")
-		.attr("width", width + margin.left + margin.right)
-	    .attr("height", height + margin.top + margin.bottom)
-	.append("g")
-	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            var y = d3.scale.linear()
+                .range([height, 0]);
 
-	d3.tsv("data.tsv", type, function(error, data) {
-	  x.domain(data.map(function(d) { return d.date; }));
-	  y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+            console.log(y)
 
-		chart.append("g")
-	    	.attr("class", "x axis")
-	    	.attr("transform", "translate(0," + height + ")")
-	    	.call(xAxis);
 
-	  chart.append("g")
-	      .attr("class", "y axis")
-	      .call(yAxis);
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
 
-	  chart.selectAll(".bar")
-	      .data(data)
-	    .enter().append("rect")
-	      .attr("class", "bar")
-	      .attr("x", function(d) { return x(d.date); })
-	      .attr("y", function(d) { return y(d.temperature); })
-	      .attr("height", function(d) { return height - y(d.value); })
-	      .attr("width", x.rangeBand());
-	});
+            console.log(xAxis)
 
-	function type(d) {
-	  d.value = +d.temperature; // coerce to number
-	  return d;
-	});
+            var yAxis = d3.svg.axis()
+                .scale(y)
+                .orient("left");
 
+            console.log(yAxis)
+
+            var chart = d3.select(".chart")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+                console.log(chart)
+
+                x.domain(data.map(function(d) {return d.date; }));
+                y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+
+                chart.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0, " + height + ")")
+                    .call(xAxis);
+
+                chart.append("g")
+                    .attr("class", "y axis")
+                    .call(yAxis);
+
+                chart.selectAll(".bar")
+                    .data(data)
+                  .enter().append("rect")
+                    .attr("class","bar")
+                    .attr("x", function(d) { return x(d.date); })
+                    .attr("y", function(d) {return y(d.temperature); })
+                    .attr("height", function(d) {return height - y(d.date); })
+                    .attr("width", x.rangeBand());
+            });
+
+            function type(d) {
+            d.value = +d.temperature; // coerce to number
+            return d;
+            }
 
